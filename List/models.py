953d1c8bf231eeb881f7c5c17f2be37fed5d5296
조연수 <duath64@soongsil.ac.kr>
2023-08-02 18:05:53 +0900

    feat : db에 기한 추가

--- List/models.py
+++ List/models.py
@@ -9,4 +9,5 @@ class Todo(models.Model):
     done = models.BooleanField(default=False)
     memo = models.TextField(default=None)
     createDate = models.DateTimeField(auto_now_add=True)
+    dueDate = models.CharField(max_length=10, default=None)
     info = models.ForeignKey(Info, to_field='user_id', max_length=50, on_delete=CASCADE)
